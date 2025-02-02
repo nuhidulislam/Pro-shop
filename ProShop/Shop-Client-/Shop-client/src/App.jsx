4113defba44b49ad371b67e4ee74201ca43b24fd
nuhidulislam <nuhidulislam@gmail.com>
2025-02-02 21:46:23 +0600

    I change my file name

--- ProShop/Shop-Client-/Shop-client/src/App.jsx
+++ ProShop/Shop-Client-/Shop-client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-2xl'>My Name is Alauddin This is my last Effort. </h1>
+      <h1 className='text-2xl'>My Name is Hero Alauddin Khan  This is my last Effort. </h1>
     </>
   )
 }
